328e7684911efba8e15cf376d90e0e5870f19743
RediatDev <rediat_ta@ch.iitr.ac.in>
2025-11-10 15:52:03 +0530

    all modification are done

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import { Navbar, Nav, Container, Button } from "react-bootstrap";
-// import { Link, useNavigate } from "react-router-dom";
-// import useAuthUser from "react-auth-kit/hooks/useAuthUser";
-// import useSignOut from "react-auth-kit/hooks/useSignOut";
-// function NavigationBar() {
-//   const navigate = useNavigate();
-//   const auth = useAuthUser();
-//   const signOut = useSignOut();
-//   const handleLogout = () => {
-//     signOut();
-//     navigate("/logIn");
-//   };
-
-//   return (
-//     <Navbar bg="dark" data-bs-theme="dark">
-//       <Container>
-//         <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
-//         <Nav className="me-auto">
-//           <Nav.Link as={Link} to="/howItWork">How it work</Nav.Link>
-//           <Nav.Link as={Link} to="/manual">Manuals</Nav.Link>
-//           <Nav.Link as={Link} to="/videos">videos</Nav.Link>
-//         </Nav>
-//         {
-//           !auth? (""):(<Button variant="outline-light" onClick={handleLogout}>
-//             Logout
-//           </Button>)
-//         }
-        
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default NavigationBar;
-
 "use client"
 
 import { useState } from "react"
@@ -76,15 +40,19 @@ function NavigationBar() {
         </button>
 
         <div className={`${styles.navLinks} ${isMenuOpen ? styles.active : ""}`}>
-         
-          <Link to="/manual" className={styles.navLink} onClick={handleNavClick}>
-            Manuals
-          </Link>
-          <Link to="/videos" className={styles.navLink} onClick={handleNavClick}>
-            Videos
-          </Link>
-           <Link to="/howItWork" className={styles.navLink} onClick={handleNavClick}>
-         About
+          {auth ? (
+            <>
+              <Link to="/manual" className={styles.navLink} onClick={handleNavClick}>
+                Manuals
+              </Link>
+              <Link to="/videos" className={styles.navLink} onClick={handleNavClick}>
+                Videos
+              </Link>
+            </>
+          ) : null}
+
+          <Link to="/howItWork" className={styles.navLink} onClick={handleNavClick}>
+            About
           </Link>
         </div>
 
